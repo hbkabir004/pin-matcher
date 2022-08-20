4b03eb93fa1656846da2da38211a2cc83f4cccfb
ABIR <hbkabir004@gmail.com>
2022-08-20 22:18:27 +0600

    final commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-document.getElementById('btn-generate').addEventListener('click', function(){
+function getPin(){
     let pin = Math.round(Math.random()*10000)+'';
     if(pin.length == 4){
         document.getElementById('generated-pin').value = Number(pin);
@@ -6,27 +6,53 @@ document.getElementById('btn-generate').addEventListener('click', function(){
     else{
         pin = Math.round(Math.random()*10000);
     }
+    return pin;
+}
+
+
+
+document.getElementById('btn-generate').addEventListener('click', function(){
+    getPin();
 })
 
 document.getElementById('number').addEventListener('click', function(event){
     let number = event.target.innerText;
     let previousNumber = document.getElementById('typed-pin').value;
-    console.log(number);
+    
+    let newNumber;
     if(isNaN(number)){
-        if(number === 'C'){
-            number = '';
-            document.getElementById('typed-pin').value = number;
-        }
-        else if(number === '<'){
-           const digits = previousNumber.split('');
-           digits.pop();
-           const remainedDigits = digits.join('') 
-            document.getElementById('typed-pin').value = remainedDigits;
-        }
+    if(number === 'C'){
+        number = '';
+        document.getElementById('typed-pin').value = number;
     }
+    else if(number === '<'){
+       const digits = previousNumber.split('');
+       digits.pop();
+       const remainedDigits = digits.join('') 
+        document.getElementById('typed-pin').value = remainedDigits;
+    }
+}
     else{
         let newNumber = previousNumber + number;
         document.getElementById('typed-pin').value = newNumber;
     }
+    return newNumber;
     
+})
+
+document.getElementById('btn-submit').addEventListener('click', function(){
+    const generatedPin = document.getElementById('generated-pin').value;
+    const typedNumber = document.getElementById('typed-pin').value;
+    const match = document.getElementById('match');
+    const error = document.getElementById('error');
+    if(generatedPin === typedNumber){
+        match.style.display = 'block';
+        error.style.display = 'none';
+
+    }
+    else{
+        error.style.display = 'block';
+        match.style.display = 'none';
+       
+    }
 })
